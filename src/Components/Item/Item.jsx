6af4c467f1bd3f773dabe05a1eb6c7f233e5e72c
Aleromeropedrin <alero@montevideo.com.uy>
2023-12-08 16:02:33 -0300

    detalles articulos color de texto

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const Item = ({ item }) => {
   return (
     
-      <Link to= {'/item/' + item.id} className= 'text-decoration-none' >
+    <Link to= {'/item/' + item.id} className= 'text-decoration-none' >
     <div className='container'>
       <div className='card border border-0'></div>
         <img src={item.imagen} className='card-img-top' alt={item.nombre} />
